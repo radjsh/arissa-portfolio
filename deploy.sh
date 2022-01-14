885042f9c60111e1d6b54c9028b314bcbcc701cf
arissa <arissa@arissas-mac.local>
2022-01-14 16:54:13 +0800

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env sh
-
 # abort on errors
 set -e
-
 # build
 npm run build
-
 # navigate into the build output directory
 cd dist
-
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:radjsh/arissa-portfolio.git main:gh-pages
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:radjsh/arissa-portfolio.git main:gh-pages
-
-cd -
+git push -f git@github.com:radjsh/arissa-portfolio.git master:gh-pages
