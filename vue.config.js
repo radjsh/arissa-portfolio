196b62f2625daac46370cbfa704ba1dd6b6bcd88
arissa <arissa@arissas-mac.local>
2026-03-13 17:53:18 +0800

    fixed tab title to arissa's portfolio

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   publicPath: "/",
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = "arissa's portfolio"
+      return args
+    })
+  }
 };
